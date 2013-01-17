01367b20274445e410a568b464bcf3e750d0d422
grindhold <in@valid.invalid>
2013-01-17 09:46:44 +0100

    Revert "added functionality for completely solving the riddle in an instant"
    This reverts commit e895b4be7bfeac0267287196f675dda65b3bb9de.

--- main.c
+++ main.c
@@ -29,39 +29,26 @@ int main()
 	unsigned char x;
 	unsigned char* eingabe_henker=NULL;
 	unsigned char* eingabe_reveal=NULL;
-	unsigned char* eingabe_solve_attempt=NULL;
 	eingabe2(&eingabe_henker);
 	if (fillchar2(&eingabe_reveal,getlength(eingabe_henker),empty_placeholder,space)==0)
 		return 1;
 	printf("%s\n",eingabe_reveal);
 	while (tries>0)
 	{
-		printf("\t1) Ich moechte noch nen Buchstaben\n\t2) Ich moechte loesen!\n\n");
-		x=mehrfachauswahl();
-		if (x == '1'){
-			x=einlesen_char();
 
-			if(is_in2(eingabe_henker,eingabe_reveal,x)==0)
-			{
-				tries--;
-			}
-			if (is_in_check(eingabe_reveal,empty_placeholder)==0)
-				break;
-			
-		} else if (x == '2') {
-			eingabe_solve(&eingabe_solve_attempt);
-			if (strcmp(eingabe_henker,eingabe_solve_attempt)==0){
-				break;
-			}else{
-				tries-=2;
-			}
+		x=einlesen_char();
+
+		if(is_in2(eingabe_henker,eingabe_reveal,x)==0)
+		{
+			tries--;
 		}
 		printf("Noch %i Versuche.\n",tries);
 		printf("%s\n",eingabe_reveal);
+		if (is_in_check(eingabe_reveal,empty_placeholder)==0)
+			break;
 	}
 
 	printf("Das Wort war: %s\n",eingabe_henker);
-	printf("Der Spieler hatte noch: %i Versuche.\n" , tries);
 	if(tries>=1)
 		printf("Die Spieler haben gewonnen\n");
 	else
